41464d9e3d5702ed1f9806d2756d3b6d702c2d22
Oleksandr Kostiuchenko <aleksandrkostucenko11@gmail.com>
2024-12-24 20:35:01 +0100

    add-sessionstorage

--- js/script.js
+++ js/script.js
@@ -10,18 +10,46 @@ const toDoList = document.querySelector('.to-do-list');
 const inProgresslist = document.querySelector('.in-progress-list');
 const completedList = document.querySelector('.completed-list');
 
+//* Renovate tasks
+const fillTasks = () => {
+    try {
+        if(sessionStorage.length === 0){
+            return;
+        }
+    
+        const TasksFromSS = JSON.parse(sessionStorage.getItem('tasks')).join('');
+        console.log(TasksFromSS)
+    
+        toDoList.insertAdjacentHTML('beforeend', TasksFromSS);
+    } catch(err){
+        console.log(err);
+    }
+}
+
+fillTasks();
+
 //* Function Add
 const createTaskTemplate = () => {
     return `<li class="list-item">📌${inputTask.value}</li>`;
 } 
 
+const HTMLtasksArr = [];
 //* Add event listeners Add btn
 addBtn.addEventListener('click', event => {
     if(inputTask.value.trim() !== ''){
-        toDoList.insertAdjacentHTML('beforeend',createTaskTemplate());
+        const HTMLTask = createTaskTemplate();
+        toDoList.insertAdjacentHTML('beforeend', HTMLTask);
+
+        HTMLtasksArr.push(HTMLTask);
+        sessionStorage.setItem(`tasks`, JSON.stringify(HTMLtasksArr));
+
         inputTask.value = '';
     }
 })
+
+// document.addEventListener('load', event => {
+//     toDoList.insertAdjacentHTML('beforeend', sessionStorage.forEach(el => sessionStorage.getItem(el)));
+// })
 
 //* Add event listeners to-do-list selected
 toDoList.addEventListener('click', event => {
@@ -36,6 +64,8 @@ toDoList.addEventListener('click', event => {
 //* Add event listeners delete BTN
 deleteBtn.addEventListener('click', event => {
     const selectedItem = document.querySelector('.selected');
+    
+
     selectedItem.remove();
 })
 
@@ -43,6 +73,8 @@ deleteBtn.addEventListener('click', event => {
 recycleBtn.addEventListener('click', event => {
     const selectedItems = document.querySelectorAll('.list-item');
     selectedItems.forEach(el => el.remove());
+
+    sessionStorage.removeItem('tasks');
 })
 
 //* Add event listeners in-progress-btn
@@ -87,11 +119,14 @@ completedList.addEventListener('click', event => {
 })
 
 //* Add event listeners to selected items for modal
+
 toDoList.addEventListener('dblclick', event => {
     const instance = basicLightbox.create(`
         <div class="modal">
             <p class="modal-text">${event.target.textContent}</p>
-            <p class="description-text"></p>
+            <p class="description-text">
+                ${localStorage.getItem(`${event.target.textContent}`)}
+            </p>
 
             <input class="description-input" placeholder="Add description">
             <button class="add-description-btn" type="button">Add</button>
@@ -107,8 +142,12 @@ toDoList.addEventListener('dblclick', event => {
             const descriptionText = instance.element().querySelector('.description-text');
 
             descriptionBtn.addEventListener('click', () => {
-                descriptionText.textContent = descriptionInput.value;
-                descriptionInput.value = '';
+                if(descriptionInput.value.trim() !== ''){
+                    localStorage.setItem(`${event.target.textContent}`, `${descriptionInput.value}`);
+                    descriptionText.textContent = localStorage.getItem(`${event.target.textContent}`); 
+
+                    descriptionInput.value = '';
+                }
             });
         }
     })
